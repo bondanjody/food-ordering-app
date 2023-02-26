555df19b4599dfe2332ae966c9bbe0c0347449ac
bondanjody <bondanjodys@gmail.com>
2023-02-26 19:52:09 +0700

    Menambahkan komponen CartIcon yang berisi svg gambar troli. Membuat component HeaderCartButton yang khusus menangani button cart pada header (gambar troli dan jumlah pembelian yang masih berupa data dummy). Mengimpor component HeaderCartButton pada component Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './Header.module.css';
 import mealPics from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>Bondan Meals</h1>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={mealPics} alt="mealpics.jpg" />

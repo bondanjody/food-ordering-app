eb6e427e61fd856c1c150f2c9a4872f911ed70e9
bondanjody <bondanjodys@gmail.com>
2023-02-27 07:44:52 +0700

    Menambahkan component Card lalu mengimpornya di component AvailableMeals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -30,9 +31,11 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
     return <section className={classes.meals}>
-        <ul>
-            {mealsList}
-        </ul>
+        <Card>
+            <ul>
+                {mealsList}
+            </ul>
+        </Card>
     </section>
 }
 

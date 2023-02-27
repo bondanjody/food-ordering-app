99e289d0783ebe4230ee54300be0f65dcb60a9a2
bondanjody <bondanjodys@gmail.com>
2023-02-27 09:29:28 +0700

    Menambahkan component Cart yang masih berisi dummy data.

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,23 @@
+import classes from './Cart.module.css';
+
+const Cart = props => {
+    const cartItem = <ul className={classes['cart-items']}>
+        {[
+            {id: 'c1', name: 'Sushi', amount: 2, price: 12.99}
+        ].map(item => <li>{item.name}</li>)}
+    </ul>;
+
+    return <div>
+        {cartItem}
+        <div className={classes.total}>
+            <span>Total amount</span>
+            <span>35.68</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button--alt']}>Close</button>
+            <button className={classes.button}>Order</button>
+        </div>
+    </div>
+};
+
+export default Cart;

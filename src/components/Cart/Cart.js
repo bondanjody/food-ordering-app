1281bc08d05022545c3da6d7a2174442405acc7c
bondanjody <bondanjodys@gmail.com>
2023-02-28 11:47:01 +0700

    Menambahkan file CartItem.js dan CartItem.module.css. Lalu memanggil component CartItem pada function map di component Cart. Tidak lupa memberikan props untuk component CartItem, serta membuat function cartItemRemove dan cartItemAdd yang masih belum berfungsi. Sehingga tampilan pada Cart sudah cukup menarik, meskipun tombol + dan - belum dapat berfungsi.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,39 @@
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = props => {
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemove = id => {
+
+    }
+
+    const cartItemAdd = item => {
+
+    }
+
     const cartItem = <ul className={classes['cart-items']}>
-        {[
-            {id: 'c1', name: 'Sushi', amount: 2, price: 12.99}
-        ].map(item => <li>{item.name}</li>)}
+        {
+            cartCtx.items.map(item => <li><CartItem  key={item.id} amount={item.amount} price={item.price} name={item.name} onRemove={cartItemRemove.bind(null, item.id)} onAdd={cartItemAdd.bind(null, item)} /></li>)
+            }
     </ul>;
 
     return <Modal onHideCart={props.onHideCart}>
         {cartItem}
         <div className={classes.total}>
             <span>Total amount</span>
-            <span>35.68</span>
+            <span>{totalAmount}</span>
         </div>
         <div className={classes.actions}>
             <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-            <button className={classes.button}>Order</button>
+            {hasItems && <button className={classes.button}>Order</button>}
         </div>
     </Modal>
 };

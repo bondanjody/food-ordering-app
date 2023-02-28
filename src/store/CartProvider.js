5692652db9a0ce7d504f672167569e350a509156
bondanjody <bondanjodys@gmail.com>
2023-02-28 07:47:56 +0700

    Membuat cart-context dan CartProvider component. Mengatur nilai yang digunakan pada cart-context. Membungkus komponen children yang ada di component App dengan CartProvider.

---
+++ src/store/CartProvider.js
@@ -0,0 +1,23 @@
+import CartContext from "./cart-context";
+
+const CartProvider = props => {
+    const addItemToCartHandler = () => {
+
+    }
+
+    const removeItemFromCartHandler = () => {
+
+    }
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    }
+    return <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+}
+
+export default CartProvider;
